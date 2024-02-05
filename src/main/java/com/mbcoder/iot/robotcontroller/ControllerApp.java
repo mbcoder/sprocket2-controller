6b38887126ee38863334c29e9e6d30865e54d952
mbcoder <mbaird@esri.com>
2024-02-05 18:05:00 +0000

    Adding some UI/ logic for drawing and playing routes.

--- src/main/java/com/mbcoder/iot/robotcontroller/ControllerApp.java
+++ src/main/java/com/mbcoder/iot/robotcontroller/ControllerApp.java
@@ -1,5 +1,17 @@
 package com.mbcoder.iot.robotcontroller;
 
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.PointCollection;
+import com.esri.arcgisruntime.geometry.Polyline;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.symbology.MultilayerPointSymbol;
+import com.esri.arcgisruntime.symbology.PictureMarkerSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.symbology.SymbolLayer;
 import java.net.Socket;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,6 +20,10 @@ import java.net.http.HttpResponse;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
+import java.util.List;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.TrustManager;
@@ -26,6 +42,19 @@ import javafx.stage.Stage;
 public class ControllerApp extends Application {
 
   private MapView mapView;
+
+  private Point currentLocation;
+  private double currentBearing;
+  private Graphic currentGraphic;
+  PictureMarkerSymbol arrowMarker;
+
+  PointCollection routePoints;
+  GraphicsOverlay routeGraphicsOverlay;
+  SimpleLineSymbol lineSymbol;
+
+  private enum modes {SET_CURRENT, DRAW_ROUTE, IGNORE_CLICK};
+
+  private modes appMode = modes.IGNORE_CLICK;
 
   private final X509ExtendedTrustManager trustManager = new X509ExtendedTrustManager() {
     @Override
@@ -82,6 +111,16 @@ public class ControllerApp extends Application {
     // create a MapView to display the map and add it to the stack pane
     mapView = new MapView();
 
+    // graphics overlay for route
+    routeGraphicsOverlay = new GraphicsOverlay();
+    mapView.getGraphicsOverlays().add(routeGraphicsOverlay);
+
+    // empty points collection for route
+    routePoints = new PointCollection(SpatialReferences.getWebMercator());
+
+    // line marker for route
+    lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.RED, 3f);
+
     borderPane.setCenter(mapView);
 
     MobileMapPackage mobileMapPackage = new MobileMapPackage("EdinburghOffice.mmpk");
@@ -96,27 +135,200 @@ public class ControllerApp extends Application {
 
     VBox buttonVbox = new VBox();
 
+    Button btnSetInitLocation = new Button("Current location");
+    btnSetInitLocation.setOnAction(event -> {
+      System.out.println("setting current location");
+      appMode = modes.SET_CURRENT;
+    });
+
+    Slider sliderDirection = new Slider(-180, 180, 0);
+    sliderDirection.valueProperty().addListener(event -> {
+      currentBearing = sliderDirection.getValue();
+
+      // if the marker symbol exists update the direction
+      if (arrowMarker!=null) {
+        arrowMarker.setAngle((float) currentBearing);
+      }
+    });
+
+    Button btnDrawRoute = new Button("Draw route");
+    btnDrawRoute.setOnAction(event -> {
+      appMode = modes.DRAW_ROUTE;
+
+    });
+
+    Button btnClearRoute = new Button("Clear route");
+    btnClearRoute.setOnAction(event -> {
+      appMode = modes.DRAW_ROUTE;
+
+      // clear route but add the first point to start a new one
+      if (routePoints.size() > 0) {
+        routePoints.clear();
+        routePoints.add(currentLocation);
+        routeGraphicsOverlay.getGraphics().clear();
+
+        //reinstate the initial current bearing
+        currentBearing = sliderDirection.getValue();
+      }
+    });
+
+    Button btnFinishRoute = new Button("Finish route");
+    btnFinishRoute.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
+    });
+
+    Button btnPlayRoute = new Button("Play route");
+    btnPlayRoute.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
+
+      playRoute();
+    });
+
+    Button btnStopRoute = new Button("Stop route");
+    btnStopRoute.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
+    });
+
     Button btnForward = new Button("Forward 100");
     btnForward.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
       System.out.println("forward 100");
       sendCommand("forward=100");
     });
 
     Button btnForwardFail = new Button("Forward Fail");
     btnForwardFail.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
       System.out.println("forward 4000 to cause fail");
       sendCommand("forward=4000");
     });
 
     Button btnRotate = new Button("Rotate 10");
     btnRotate.setOnAction(event -> {
+      appMode = modes.IGNORE_CLICK;
       System.out.println("rotate 10");
       sendCommand("rotate=10");
     });
 
-    buttonVbox.getChildren().addAll(btnForward, btnForwardFail, btnRotate);
+    buttonVbox.getChildren().addAll(
+        btnSetInitLocation,
+        sliderDirection,
+        btnDrawRoute,
+        btnFinishRoute,
+        btnClearRoute,
+        btnPlayRoute,
+        btnStopRoute,
+        btnForward,
+        btnForwardFail,
+        btnRotate);
+
+    // handle map click events
+    mapView.setOnMouseClicked(event -> {
+
+      Point2D clickedPoint = new Point2D(event.getX(), event.getY());
+      Point mapLocation = mapView.screenToLocation(clickedPoint);
+
+      // handle new location
+      if (appMode == modes.SET_CURRENT) {
+
+        // update current location
+        currentLocation = mapLocation;
+
+        // clear the route points and graphic
+        routePoints.clear();
+        routePoints.add(mapLocation);
+        routeGraphicsOverlay.getGraphics().clear();
+
+        // update location on screen
+        if (currentGraphic != null) {
+
+          currentGraphic.setGeometry(mapLocation);
+
+        } else {
+          // set up graphic for 1st time
+          arrowMarker = new PictureMarkerSymbol("Arrow.png");
+          arrowMarker.loadAsync();
+          arrowMarker.addDoneLoadingListener(() -> {
+            System.out.println("arrow loaded");
+
+
+            currentGraphic = new Graphic(mapLocation, arrowMarker);
+
+            GraphicsOverlay locationOverlay = new GraphicsOverlay();
+            mapView.getGraphicsOverlays().add(locationOverlay);
+
+            locationOverlay.getGraphics().add(currentGraphic);
+          });
+        }
+      }
+
+      // handle adding points to route
+      if (appMode == modes.DRAW_ROUTE) {
+        // add new points and refresh graphic
+        routePoints.add(mapLocation);
+
+        updateRouteGraphic();
+
+      }
+    });
 
     borderPane.setLeft(buttonVbox);
+  }
+
+  private void playRoute() {
+    System.out.println("playing route...");
+
+    Point originPoint = null;
+
+    // loop through the route points
+    for (Point point: routePoints) {
+      // first point do nothing, just collect the point
+      if (originPoint == null) {
+        // store the origin
+        originPoint = point;
+      } else {
+        // process the 2 points
+
+        var xDiff = point.getX() - originPoint.getX();
+        var yDiff = point.getY() - originPoint.getY();
+
+        var distance = GeometryEngine.distanceBetween(originPoint,point);
+
+        var bearing = Math.toDegrees(Math.atan(xDiff / yDiff));
+        // correct if heading South West
+        if (yDiff < 0 && xDiff < 0) {
+          bearing = bearing -180;
+        }
+
+        // correct if heading South East
+        if (yDiff < 0 && xDiff > 0) {
+          bearing = 180 + bearing;
+        }
+
+        // angle to rotate robot
+        System.out.println("current bearing " + currentBearing);
+        var rotateAngle = bearing - currentBearing;
+
+        // update the current angle as it will be after it has rotated
+        currentBearing = bearing;
+
+        System.out.println("diff x=" + xDiff + " y=" + yDiff + " bearing=" + bearing + " distance=" + distance + " rotate=" + rotateAngle);
+
+        //reset origin for next step
+        originPoint = point;
+      }
+
+    }
+
+  }
+
+  private void updateRouteGraphic() {
+    routeGraphicsOverlay.getGraphics().clear();
+
+    Polyline polyline = new Polyline(routePoints);
+    Graphic graphic = new Graphic(polyline, lineSymbol);
+    routeGraphicsOverlay.getGraphics().add(graphic);
+
   }
 
   private void sendCommand(String command) {
@@ -128,7 +340,7 @@ public class ControllerApp extends Application {
           .sslContext(sslContext)
           .build();
 
-      URI uri = URI.create("https://127.0.0.1:8080/testOne?" + command);
+      URI uri = URI.create("https://raspberrypi.local:8080/testOne?" + command);
       var request = HttpRequest
           .newBuilder()
           .uri(uri)
